a1d6ab62cdc471f37d08c04df8b25f7e68f9a0e4
Liam Young <liam.young@canonical.com>
2016-05-05 23:08:44 +0000

    Tidy up actions as per python style guide and regenerate config pon each action frun

--- actions/run-tempest.py
+++ actions/run-tempest.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
 
-import sys
-sys.path.append('lib')
-
 import os
 import subprocess
-from charmhelpers.core.hookenv import (
-    action_set,
-    config,
-    action_get,
-)
-from time import gmtime, strftime
+import sys
+import time
+sys.path.append('lib')
+
 import charm.openstack.tempest as tempest
-from charmhelpers.fetch import install_remote
+import charms.reactive as reactive
+import charmhelpers.core.hookenv as hookenv
+import charmhelpers.fetch as fetch
 
 
 def setup_git(branch, git_dir, tempest_conf):
-    conf = config()
+    conf = hookenv.config()
     if not os.path.exists(git_dir):
         git_url = conf['tempest-source']
-        install_remote(str(git_url), dest=str(git_dir), branch=str(branch),
-                       depth=str(1))
+        fetch.install_remote(str(git_url), dest=str(git_dir),
+                             branch=str(branch), depth=str(1))
     conf_symlink = git_dir + '/tempest/etc/tempest.conf'
     if not os.path.exists(conf_symlink):
         os.symlink(tempest_conf, conf_symlink)
@@ -28,7 +25,7 @@ def setup_git(branch, git_dir, tempest_conf):
 
 def execute_tox(run_dir, logfile):
     env = os.environ.copy()
-    conf = config()
+    conf = hookenv.config()
     if conf.get('http-proxy'):
         env['http_proxy'] = conf['http-proxy']
     if conf.get('https-proxy'):
@@ -39,9 +36,9 @@ def execute_tox(run_dir, logfile):
 
 
 def run_smoke_test():
-    action_args = action_get()
+    action_args = hookenv.action_get()
     branch = action_args['branch']
-    log_time_str = strftime("%Y%m%d%H%M%S", gmtime())
+    log_time_str = time.strftime("%Y%m%d%H%M%S", time.gmtime())
     tempest_git_dir = '{}/tempest-{}'.format(
         tempest.TempestCharm.TEMPEST_ROOT,
         branch
@@ -56,8 +53,11 @@ def run_smoke_test():
     run_dir = tempest_git_dir + '/tempest'
     setup_git(branch, tempest_git_dir, tempest.TempestCharm.TEMPEST_CONF)
     execute_tox(run_dir, tempest_logfile)
-    action_set(action_info)
+    hookenv.action_set(action_info)
 
 
 if __name__ == '__main__':
+    # Cloud may have different artifacts (flavors, images etc) since last run
+    # so rerun handlers file to regenerate config.
+    reactive.main()
     run_smoke_test()
