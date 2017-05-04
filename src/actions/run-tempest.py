9df9d8050107f09f9e96843317c048a17542306c
Alex Kavanagh <alex@ajkavanagh.co.uk>
2017-05-04 18:54:03 +0100

    Remove bus.discover() from the run-tempest action
    As charms.reactive 0.4.7 has landed in PyPi, the bus.discover() call is
    no longer needed to fixup the implementation of an interface (relation).
    This just cleans the code up.
    
    Change-Id: I83021c821d3c09aa64832efe1185b34c9e4dd3b8

--- src/actions/run-tempest.py
+++ src/actions/run-tempest.py
@@ -4,7 +4,6 @@ sys.path.append('lib')
 
 import charm.openstack.tempest as tempest
 import charms.reactive as reactive
-import charms.reactive.bus as bus
 import charmhelpers.core.hookenv as hookenv
 
 # Make sure that reactive is bootstrapped and all the states are setup
@@ -14,11 +13,6 @@ basic.bootstrap_charm_deps()
 basic.init_config_states()
 
 if __name__ == '__main__':
-    # charms.reactive 0.4.6 moved the auto discovery of interfaces out of the
-    # module load and into the main() function.  Actions don't want to run the
-    # main (as we don't want the handlers/hooks to run) so this just finds the
-    # interfaces to the '.from_state()' function will get the interface object.
-    bus.discover()
     identity_int = reactive.RelationBase.from_state('identity-admin.available')
     if identity_int is None:
         # The interface isn't connected, so we can't do this yet
