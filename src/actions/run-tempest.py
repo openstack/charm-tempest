844da5667598cd9e7185651be5dca93ee2634ab4
Alex Kavanagh <alex@ajkavanagh.co.uk>
2018-05-24 15:46:17 +0100

    Rebuild charm to pick up latest layer-openstack/fix venv action bug
    This is needed to work around a setuptool_scm bug.  Also, the action
    file had to be re-arranged slightly to ensure the venv is set up
    before anything from the wheelhouse is imported.
    
    Change-Id: Ie913fb5946afdaf1bfb7fd9df8b5d053da82c535

--- src/actions/run-tempest.py
+++ src/actions/run-tempest.py
@@ -2,15 +2,16 @@
 import sys
 sys.path.append('lib')
 
-import charm.openstack.tempest as tempest
-import charms.reactive.relations as relations
-import charmhelpers.core.hookenv as hookenv
-
 # Make sure that reactive is bootstrapped and all the states are setup
 # properly
 from charms.layer import basic
 basic.bootstrap_charm_deps()
 basic.init_config_states()
+
+import charm.openstack.tempest as tempest
+import charms.reactive.relations as relations
+import charmhelpers.core.hookenv as hookenv
+
 
 if __name__ == '__main__':
     identity_int = relations.endpoint_from_flag('identity-admin.available')
