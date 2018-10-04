bba9749806f59036f67be9d6c9bdd51b61d16f41
Ryan Beisner <ryan.beisner@canonical.com>
2018-10-04 10:48:27 -0500

    Update requirements
    Also remove tests/charm-helpers if present, in favor
    of the pip-installed charm-helpers.
    
    Change-Id: I1ac0cbf16711fb6e93c42b2b25e010cf0876a98d

--- unit_tests/__init__.py
+++ unit_tests/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import mock
 import sys
 
 sys.path.append('src')
@@ -20,3 +21,37 @@ sys.path.append('src/lib')
 # Mock out charmhelpers so that we can test without it.
 import charms_openstack.test_mocks  # noqa
 charms_openstack.test_mocks.mock_charmhelpers()
+
+
+# Mock out OpenStack clients the tempest charm imports so they are not
+# required for testing.
+glanceclient = mock.MagicMock()
+sys.modules['glanceclient'] = glanceclient
+
+keystoneauth1 = mock.MagicMock()
+sys.modules['keystoneauth1'] = keystoneauth1
+sys.modules['keystoneauth1.identity'] = keystoneauth1.identity
+sys.modules['keystoneauth1.identity.v1'] = keystoneauth1.identity.v1
+sys.modules['keystoneauth1.identity.v2'] = keystoneauth1.identity.v2
+sys.modules['keystoneauth1.session'] = keystoneauth1.session
+
+keystoneclient = mock.MagicMock()
+sys.modules['keystoneclient'] = keystoneclient
+sys.modules['keystoneclient.auth'] = keystoneclient.auth
+sys.modules['keystoneclient.auth.identity'] = keystoneclient.auth.identity
+sys.modules['keystoneclient.auth.identity.v3'] = (
+    keystoneclient.auth.identity.v3)
+sys.modules['keystoneclient.v2_0'] = keystoneclient.v2_0
+sys.modules['keystoneclient.v2_0.client'] = keystoneclient.v2_0.client
+sys.modules['keystoneclient.v3'] = keystoneclient.v3
+sys.modules['keystoneclient.v3.client'] = keystoneclient.v3.client
+sys.modules['keystoneclient.session'] = keystoneclient.session
+
+neutronclient = mock.MagicMock()
+sys.modules['neutronclient'] = neutronclient
+sys.modules['neutronclient.v2_0'] = neutronclient.v2_0
+sys.modules['neutronclient.v2_0.client'] = neutronclient.v2_0.client
+
+novaclient = mock.MagicMock()
+sys.modules['novaclient'] = novaclient
+sys.modules['novaclient.client'] = novaclient.client
